664634bf63d9471ff24c9c981d01a799d5a00e7d
korenev6052 <korenev6052@gmail.com>
2020-11-03 01:17:53 +0300

    l-2: handlebars

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const express = require('express');
-const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 });
 
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'routes', 'home.html'));
+  res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'routes', 'about.html'));
+  res.render('about');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
